33698a0bd3a6b0afe326ac8b9c0e773ee3208291
va164 <victor.ahluwalia@gmail.com>
2016-04-28 14:35:38 -0400

    first commit

---
+++ js/script.js
@@ -0,0 +1,10 @@
+$('document').ready(function() {
+	console.log("page loaded");
+
+	/*$('document')keypress(function (e)
+		if (event.which == 13) {
+			event.preventDefault();
+			$("form").submit()
+		}
+		) */
+}); 
